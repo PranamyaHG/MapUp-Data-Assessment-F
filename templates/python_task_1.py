c6040625a88359d56f5af2bc359ab989543cb8a1
PranamyaHG <153181667+PranamyaHG@users.noreply.github.com>
2023-12-10 21:11:34 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -12,9 +12,19 @@ def generate_car_matrix(df)->pd.DataFrame:
         pandas.DataFrame: Matrix generated with 'car' values, 
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
-    # Write your logic here
+# Load the dataset into a DataFrame
+df = pd.read_csv('dataset-1.csv')
 
-    return df
+# Create a pivot table
+pivot_df = df.pivot_table(index='id_1', columns='id_2', values='car', fill_value=0)
+
+# Set diagonal values to zero
+for i in range(min(pivot_df.shape)):
+    pivot_df.iloc[i, i] = 0
+
+print(pivot_df)
+
+return df
 
 
 def get_type_count(df)->dict:
@@ -27,9 +37,29 @@ def get_type_count(df)->dict:
     Returns:
         dict: A dictionary with car types as keys and their counts as values.
     """
-    # Write your logic here
+   # Create a new categorical column 'car_type' based on values in 'car' column
+    conditions = [
+        (df['car'] <= 15),
+        (df['car'] > 15) & (df['car'] <= 25),
+        (df['car'] > 25)
+    ]
+    choices = ['low', 'medium', 'high']
+    df['car_type'] = pd.Series(
+        np.select(conditions, choices, default='Unknown'), dtype='category'
+    )
 
-    return dict()
+    # Calculate count of occurrence for each car type category
+    count_dict = df['car_type'].value_counts().sort_index().to_dict()
+    return count_dict
+
+# Load the dataset into a DataFrame
+df = pd.read_csv('dataset-1.csv')
+
+# Get the count of car types
+result = get_type_count(df)
+print(result)
+
+return dict()
 
 
 def get_bus_indexes(df)->list:
@@ -42,9 +72,22 @@ def get_bus_indexes(df)->list:
     Returns:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
-    # Write your logic here
+  # Calculate the mean value of the 'bus' column
+    mean_bus = df['bus'].mean()
 
-    return list()
+    # Identify indices where 'bus' values are greater than twice the mean value
+    bus_indexes = df[df['bus'] > 2 * mean_bus].index.tolist()
+
+    return bus_indexes
+
+# Load the dataset into a DataFrame
+df = pd.read_csv('dataset-1.csv')
+
+# Get the indices where 'bus' values are greater than twice the mean
+result = get_bus_indexes(df)
+print(result)
+
+return list()
 
 
 def filter_routes(df)->list:
@@ -57,9 +100,25 @@ def filter_routes(df)->list:
     Returns:
         list: List of route names with average 'truck' values greater than 7.
     """
-    # Write your logic here
+# Calculate the average of 'truck' grouped by 'route'
+    route_avg = df.groupby('route')['truck'].mean()
 
-    return list()
+    # Filter routes where the average is greater than 7
+    filtered_routes = route_avg[route_avg > 7].index.tolist()
+
+    # Sort the list of routes
+    filtered_routes.sort()
+
+    return filtered_routes
+
+# Load the dataset into a DataFrame
+df = pd.read_csv('dataset-1.csv')
+
+# Get the sorted list of routes where the average of 'truck' is greater than 7
+result = filter_routes(df)
+print(result)
+
+return list()
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -72,9 +131,23 @@ def multiply_matrix(matrix)->pd.DataFrame:
     Returns:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
-    # Write your logic here
+  # Modify values based on the logic provided
+    df_modified = df.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+    
+    # Round the modified values to one decimal place
+    df_modified = df_modified.round(1)
+    
+    return df_modified
 
-    return matrix
+# Assuming 'result_df' contains the resulting DataFrame from the solution to question 1
+# Replace 'result_df' with the actual resulting DataFrame obtained from question 1
+result_df = ...  # Replace this with the actual resulting DataFrame
+
+# Apply the multiplication logic and rounding to the DataFrame
+modified_df = multiply_matrix(result_df)
+print(modified_df)
+
+ return matrix
 
 
 def time_check(df)->pd.Series:
